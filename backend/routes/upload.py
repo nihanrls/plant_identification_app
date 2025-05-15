2ae024574573d9f94313c7ebe5e69a08afb2e48c
Nihan Arslan <aarslannihan@gmail.com>
2025-05-15 23:15:48 +0300

    bitkileri tanıması için hazır bir api eklendi

--- backend/routes/upload.py
+++ backend/routes/upload.py
@@ -2,6 +2,10 @@ from flask import Blueprint, request, jsonify
 from werkzeug.utils import secure_filename
 import os
 import time
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 upload_bp = Blueprint("upload", __name__)
 
@@ -13,6 +17,9 @@ ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg"}
 #Extension control
 def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+PLANETNET_API_KEY = os.getenv("PLANTNET_API_KEY")
+PLANETNET_API_URL = "https://my-api.plantnet.org/v2/identify/all"
 
 @upload_bp.route("/upload", methods = ["POST"])
 
@@ -32,10 +39,35 @@ def upload_file():
         save_path = os.path.join(UPLOAD_FOLDER, filename)
         file.save(save_path)
         
-        return jsonify({
-            "message": "Görsel başarıyla yüklendi",
-            "filename": filename,
-            "path": save_path
-        }), 200
+        with open(save_path, "rb") as img:
+            files = {"images": img}
+            data = {
+                "organs": "leaf",
+            }
+            response = requests.post(
+                f"{PLANETNET_API_URL}?api-key={PLANETNET_API_KEY}",
+                files = files,
+                data = data
+            )
+        
+        if response.status_code == 200:
+            result = response.json()
+            if result.get("results"):
+                best_match = result["results"][0]["species"]["scientificNameWithoutAuthor"]
+                return jsonify({
+                    "message": "Görsel yüklendi ve tanındı",
+                    "filename": filename,
+                    "plant_name": best_match
+                }), 200
+            else:
+                return jsonify({
+                    "message": "Görsel yüklendi fakat tanıma yapılamadı",
+                    "filename": filename
+                }), 200
+        else:
+            return jsonify({
+                "error": "PlantNet API isteği başarısız",
+                "status": response.status_code
+            }), 500
         
     return jsonify({"error": "Geçersiz dosya türü. Sadece PNG/JPG/JPEG destekleniyor."}), 400
